bd8d0682c2b3b8d3fc0b71f5180f2b9e2eacf7f3
KingSeokJin <sjin7629@gmail.com>
2025-12-08 16:06:04 +0900

    profile UI

--- src/app/ui/PostCard.tsx
+++ src/app/ui/PostCard.tsx
@@ -1,6 +1,6 @@
 const PostCard = () => {
   return (
-    <div className="h-[515px] w-[538px] rounded-[14px] bg-white">PostCard</div>
+    <div className="h-[515px] w-[530px] rounded-[14px] bg-white">PostCard</div>
   );
 };
 
